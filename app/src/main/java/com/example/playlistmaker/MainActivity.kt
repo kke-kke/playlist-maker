62160910405aa726b43ec9fa6f54d6e876f6e759
kk <ks8niko@gmail.com>
2024-05-08 21:34:48 +0300

    тосты для домашнего задания 8_2

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,10 +2,26 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.search)
+        val libraryButton = findViewById<Button>(R.id.library)
+        val settingsButton = findViewById<Button>(R.id.settings)
+
+        searchButton.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Нажали на поиск", Toast.LENGTH_SHORT).show()
+        }
+        libraryButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на библиотеку", Toast.LENGTH_SHORT).show()
+        }
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на настройки", Toast.LENGTH_SHORT).show()
+        }
     }
 }
