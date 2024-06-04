79d8b048c53d181c644e63387cc89dd25199bdff
Ksenia Kovaleva <ks8niko@gmail.com>
2024-06-04 23:47:17 +0300

    добавлена обработка сохраненного состояния; клавиатура прячется после очистки поля

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,23 +1,40 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.inputmethod.InputMethodManager
 import android.widget.ImageButton
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.textfield.TextInputEditText
 
+
 class SearchActivity : AppCompatActivity() {
+    private var searchValue: String = DEFAULT_VALUE
+
+    private lateinit var inputEditText: TextInputEditText
+
+    companion object {
+        const val SEARCH_TEXT: String = ""
+        const val DEFAULT_VALUE = ""
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
         val backButton = findViewById<ImageButton>(R.id.backArrowSearch)
-        val inputEditText = findViewById<TextInputEditText>(R.id.searchBar)
+        inputEditText = findViewById(R.id.searchBar)
 
         // кнопка "назад"
         backButton.setOnClickListener {
             onBackPressedDispatcher.onBackPressed()
+        }
+
+        // восстанавливается сохраненное состояние, если есть
+        if (savedInstanceState != null) {
+            searchValue = savedInstanceState.getString(SEARCH_TEXT, DEFAULT_VALUE)
+            inputEditText.setText(searchValue)
         }
 
         // строка поиска
@@ -27,14 +44,33 @@ class SearchActivity : AppCompatActivity() {
             }
 
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-                // empty
+                searchValue = s.toString()
             }
 
             override fun afterTextChanged(s: Editable?) {
-                // empty
+                // клавиатура прячется после очистки поля
+                if (s.isNullOrEmpty()) {
+                    hideKeyboard()
+                }
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(SEARCH_TEXT, searchValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        searchValue = savedInstanceState.getString(SEARCH_TEXT, DEFAULT_VALUE)
+        inputEditText.setText(searchValue)
+    }
+
+    private fun hideKeyboard() {
+        val mgr = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+        mgr.hideSoftInputFromWindow(inputEditText.windowToken, 0)
     }
 
 }
