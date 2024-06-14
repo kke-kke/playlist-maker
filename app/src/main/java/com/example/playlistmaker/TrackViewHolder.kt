9a14975e21137f44e298f6c50ffce80188c3441b
Ksenia Kovaleva <ks8niko@gmail.com>
2024-06-14 14:45:27 +0300

    работа с recycler view

---
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -0,0 +1,25 @@
+package com.example.playlistmaker
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
+import com.bumptech.glide.request.RequestOptions
+import com.example.playlistmaker.databinding.SearchResultBinding
+
+class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    private val binding: SearchResultBinding = SearchResultBinding.bind(itemView)
+
+    fun bind(track: Track) {
+        binding.songNameTextView.text = track.trackName
+        binding.songArtistTextView.text = track.artistName
+        binding.songLengthTextView.text = track.trackTime
+        Glide.with(itemView.context)
+            .load(track.artworkUrl100)
+            .placeholder(R.drawable.ic_mock_cover)
+            .fitCenter()
+            .apply(RequestOptions.bitmapTransform(RoundedCorners(4)))
+            .into(binding.albumCoverImage)
+    }
+}
