36e98efacf0dfa3c0d0080f5f0c725227d730282
Ksenia Kovaleva <ks8niko@gmail.com>
2024-07-03 14:24:22 +0300

    добавлен serializedName

--- app/src/main/java/com/example/playlistmaker/Track.kt
+++ app/src/main/java/com/example/playlistmaker/Track.kt
@@ -1,8 +1,14 @@
 package com.example.playlistmaker
 
+import com.google.gson.annotations.SerializedName
+
 data class Track(
+    @SerializedName("trackName")
     val trackName: String, // Название композиции
+    @SerializedName("artistName")
     val artistName: String, // Имя исполнителя
+    @SerializedName("trackTime")
     val trackTime: String, // Продолжительность трека
+    @SerializedName("artworkUrl100")
     val artworkUrl100: String // Ссылка на изображение обложки
 )
