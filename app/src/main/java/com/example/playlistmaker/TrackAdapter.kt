105abdf5fe9f3722d0e07592319b612849b25c14
Ksenia Kovaleva <ks8niko@gmail.com>
2024-07-18 17:05:15 +0300

    список треков в адаптере

--- app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
+++ app/src/main/java/com/example/playlistmaker/TrackAdapter.kt
@@ -4,7 +4,8 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
-class TrackAdapter(private val tracks: List<Track>) : RecyclerView.Adapter<TrackViewHolder> () {
+class TrackAdapter() : RecyclerView.Adapter<TrackViewHolder> () {
+    var tracks = ArrayList<Track>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TrackViewHolder {
         val view = LayoutInflater.from(parent.context).inflate(R.layout.search_result, parent, false)
