644e0cff7bb45629321d169b5f6e395839a510f4
Ksenia Kovaleva <ks8niko@gmail.com>
2024-07-18 17:06:20 +0300

    реализован поиск и черновой вариант заглушек на неудачный поиск

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -3,28 +3,41 @@ package com.example.playlistmaker
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.view.MotionEvent
+import android.view.View
+import android.view.inputmethod.EditorInfo
 import android.view.inputmethod.InputMethodManager
 import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.textfield.TextInputEditText
-import com.google.gson.Gson
-import com.google.gson.reflect.TypeToken
-
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SearchActivity : AppCompatActivity() {
     private var searchValue: String = DEFAULT_VALUE
 
     private lateinit var inputEditText: TextInputEditText
+    private lateinit var placeholderMessage: TextView
+    private lateinit var placeholderImage: ImageView
+    private lateinit var trackAdapter: TrackAdapter
+    private var trackList = ArrayList<Track>()
+    private val networkProblem : String = getString(R.string.connection_problems) + "/n/n" +
+            getString(R.string.connection_problems_message)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
 
+        placeholderMessage = findViewById(R.id.placeholderMessage)
+        placeholderImage = findViewById(R.id.placeholderImage)
+
         // recyclerView
         val json: String = assets.open("tracks.json").bufferedReader().use { it.readText() }
-        val trackList = Gson().fromJson<ArrayList<Track>>(json, object : TypeToken<ArrayList<Track>>() {}.type)
-        val trackAdapter = TrackAdapter(trackList)
+        trackAdapter = TrackAdapter()
         val recyclerView = findViewById<RecyclerView>(R.id.searchResultRecyclerView)
         recyclerView.adapter = trackAdapter
 
@@ -54,6 +67,36 @@ class SearchActivity : AppCompatActivity() {
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
+
+        trackAdapter.tracks = trackList
+
+        // поиск на иконку поиска
+        inputEditText.setOnTouchListener { v, event ->
+            if (event.action == MotionEvent.ACTION_UP) {
+                val drawables = inputEditText.compoundDrawables
+                val startDrawable = drawables[0]
+
+                startDrawable?.let {
+                    val drawableBounds = it.bounds
+                    val drawableWidth = drawableBounds.width()
+
+                    if (event.rawX <= (inputEditText.left + drawableWidth + inputEditText.paddingStart)) {
+                        // Запуск логики поиска
+                        performSearch(inputEditText.text.toString())
+                        return@setOnTouchListener true
+                    }
+                }
+            }
+            false
+        }
+        // поиск на клавиатуре
+        inputEditText.setOnEditorActionListener { _, actionId, _ ->
+            if (actionId == EditorInfo.IME_ACTION_DONE) {
+                performSearch(inputEditText.text.toString())
+                true
+            }
+            false
+        }
     }
 
     override fun onSaveInstanceState(outState: Bundle) {
@@ -70,6 +113,52 @@ class SearchActivity : AppCompatActivity() {
     private fun hideKeyboard() {
         val mgr = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         mgr.hideSoftInputFromWindow(inputEditText.windowToken, 0)
+    }
+
+    private fun performSearch(query: String) {
+        val searchApi = SearchApi.create()
+
+        if (query.isNotEmpty()) {
+            searchApi.search(query).enqueue(object : Callback<TrackResponse> {
+                override fun onResponse(
+                    call: Call<TrackResponse>,
+                    response: Response<TrackResponse>
+                ) {
+                    if (response.code() == 200) {
+                        trackList.clear()
+                        if (response.body()?.results?.isNotEmpty() == true) {
+                            trackList.addAll(response.body()?.results!!)
+                            trackAdapter.notifyDataSetChanged()
+                        }
+                        if (trackList.isEmpty()) {
+                            showMessage(R.drawable.nothing_found, getString(R.string.nothing_found))
+                        }
+                    } else {
+                        showMessage(R.drawable.network_connection_problems, networkProblem)
+                    }
+                }
+
+                override fun onFailure(call: Call<TrackResponse>, t: Throwable) {
+                    showMessage(R.drawable.network_connection_problems, networkProblem)
+                }
+            })
+        }
+
+    }
+
+    private fun showMessage(image: Int, textMessage: String) {
+        if (textMessage.isNotEmpty()) {
+            placeholderMessage.visibility = View.VISIBLE
+            trackList.clear()
+            trackAdapter.notifyDataSetChanged()
+            placeholderMessage.text = textMessage
+            if (image != 0) {
+                placeholderImage.setImageResource(image)
+            }
+        } else {
+            placeholderMessage.visibility = View.GONE
+            placeholderImage.visibility = View.GONE
+        }
     }
 
     companion object {
