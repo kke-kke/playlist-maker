8fc18c07bed5d0ee1585fb296804b0befac46b11
Ksenia Kovaleva <ks8niko@gmail.com>
2024-09-17 13:15:36 +0300

    длина трека в формате mm:ss

--- app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
+++ app/src/main/java/com/example/playlistmaker/TrackViewHolder.kt
@@ -6,6 +6,8 @@ import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
 import com.example.playlistmaker.databinding.SearchResultBinding
+import java.text.SimpleDateFormat
+import java.util.Locale
 
 class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
@@ -15,7 +17,8 @@ class TrackViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         with(binding) {
             songNameTextView.text = track.trackName
             songArtistTextView.text = track.artistName
-            songLengthTextView.text = track.trackTime
+            songLengthTextView.text = SimpleDateFormat("mm:ss", Locale.getDefault()).format(track.trackTime)
+
             Glide.with(itemView.context)
                 .load(track.artworkUrl100)
                 .placeholder(R.drawable.ic_mock_cover)
