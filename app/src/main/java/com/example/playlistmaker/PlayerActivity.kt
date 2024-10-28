867dc2551b10f31d8dc7469716577aa97f7a5ef6
Ksenia Kovaleva <ks8niko@gmail.com>
2024-10-29 00:15:28 +0300

    создается media player, кнопка плей меняется на паузу

--- app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
+++ app/src/main/java/com/example/playlistmaker/PlayerActivity.kt
@@ -1,6 +1,9 @@
 package com.example.playlistmaker
 
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.util.Log
+import android.widget.ImageButton
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -9,6 +12,11 @@ import com.example.playlistmaker.databinding.ActivityPlayerBinding
 import java.text.SimpleDateFormat
 
 class PlayerActivity : AppCompatActivity() {
+
+    private var mediaPlayer = MediaPlayer()
+    private var playerState = STATE_DEFAULT
+    private lateinit var url: String
+    private lateinit var play: ImageButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +33,26 @@ class PlayerActivity : AppCompatActivity() {
         // получение трека из предыдущей активити
         val track = intent.getSerializableExtra("TRACK") as Track
         bindTrack(playerBinding, track)
+
+        url = track.previewUrl ?: ""
+        if (url.isNotEmpty()) {
+            preparePlayer()
+        } else {
+            Log.e("PlayerActivity", "previewUrl пустой или null.")
+        }
+
+        play = playerBinding.playButton
+        play.setOnClickListener { playbackControl() }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        pausePlayer()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mediaPlayer.release()
     }
 
     private fun bindTrack(binding: ActivityPlayerBinding, track: Track) {
@@ -47,4 +75,49 @@ class PlayerActivity : AppCompatActivity() {
 
         }
     }
+
+    private fun preparePlayer() {
+        mediaPlayer.setDataSource(url)
+        mediaPlayer.prepareAsync()
+        mediaPlayer.setOnPreparedListener {
+            play.isEnabled = true
+            play.setImageResource(R.drawable.play)
+            playerState = STATE_PREPARED
+        }
+        mediaPlayer.setOnCompletionListener {
+            play.setImageResource(R.drawable.play)
+            playerState = STATE_PREPARED
+        }
+    }
+
+    private fun startPlayer() {
+        mediaPlayer.start()
+        play.setImageResource(R.drawable.pause)
+        playerState = STATE_PLAYING
+    }
+
+    private fun pausePlayer() {
+        mediaPlayer.pause()
+        play.setImageResource(R.drawable.play)
+        playerState = STATE_PAUSED
+    }
+
+    private fun playbackControl() {
+        when(playerState) {
+            STATE_PLAYING -> {
+                pausePlayer()
+            }
+            STATE_PREPARED, STATE_PAUSED -> {
+                startPlayer()
+            }
+        }
+    }
+
+    companion object {
+        private const val STATE_DEFAULT = 0
+        private const val STATE_PREPARED = 1
+        private const val STATE_PLAYING = 2
+        private const val STATE_PAUSED = 3
+    }
+
 }
