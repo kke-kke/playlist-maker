fdddf3f5a9a526e93eec8fc0c62710bd131ebeed
Ksenia Kovaleva <ks8niko@gmail.com>
2024-11-29 14:33:07 +0300

    удалены неиспользуемые зависимости

--- app/src/main/java/com/example/playlistmaker/domain/settings/impl/SettingsInteractorImpl.kt
+++ app/src/main/java/com/example/playlistmaker/domain/settings/impl/SettingsInteractorImpl.kt
@@ -1,11 +1,8 @@
 package com.example.playlistmaker.domain.settings.impl
 
-import android.content.Context.MODE_PRIVATE
 import com.example.playlistmaker.domain.settings.api.SettingsInteractor
 import com.example.playlistmaker.domain.settings.api.SettingsRepository
 import com.example.playlistmaker.utils.App
-import com.example.playlistmaker.utils.Constants.SETTINGS_PREFERENCES
-import com.example.playlistmaker.utils.Constants.THEME_KEY
 
 class SettingsInteractorImpl(private val settingsRepository: SettingsRepository, private val app: App): SettingsInteractor {
     override fun isDarkThemeEnabled(): Boolean {
