1dccfeda79fda17107a2308b731af861458ae0b4
Ksenia Kovaleva <ks8niko@gmail.com>
2024-12-12 18:22:04 +0300

    activity заменены на fragment, добавлен bottom navigation view

--- app/src/main/java/com/example/playlistmaker/ui/main/activity/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/ui/main/activity/MainActivity.kt
@@ -1,12 +1,11 @@
 package com.example.playlistmaker.ui.main.activity
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.playlistmaker.R
 import com.example.playlistmaker.databinding.ActivityMainBinding
-import com.example.playlistmaker.ui.library.activity.LibraryActivity
-import com.example.playlistmaker.ui.search.activity.SearchActivity
-import com.example.playlistmaker.ui.settings.activity.SettingsActivity
 
 class MainActivity : AppCompatActivity() {
     private lateinit var mainBinding: ActivityMainBinding
@@ -16,18 +15,9 @@ class MainActivity : AppCompatActivity() {
         mainBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mainBinding.root)
 
-        val searchIntent = Intent(this, SearchActivity::class.java)
-        val libraryIntent = Intent(this, LibraryActivity::class.java)
-        val settingsIntent = Intent(this, SettingsActivity::class.java)
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.container_view) as NavHostFragment
+        val navController = navHostFragment.navController
 
-        mainBinding.searchButton.setOnClickListener{
-            startActivity(searchIntent)
-        }
-        mainBinding.libraryButton.setOnClickListener {
-            startActivity(libraryIntent)
-        }
-        mainBinding.settingsButton.setOnClickListener {
-            startActivity(settingsIntent)
-        }
+        mainBinding.bottomNavigationView.setupWithNavController(navController)
     }
 }
