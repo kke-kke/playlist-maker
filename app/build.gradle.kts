480b30d3eda6b9c6ddd3b9657b6c20233cd1584c
Ksenia Kovaleva <ks8niko@gmail.com>
2024-12-28 19:32:38 +0300

    добавлены корутины

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,4 +54,5 @@ dependencies {
     implementation("androidx.navigation:navigation-fragment-ktx:2.5.3")
     implementation("androidx.navigation:navigation-ui-ktx:2.5.3")
     implementation("androidx.fragment:fragment-ktx:1.5.6")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 }
