324b4789e61e0c83463adedbb78bd202e6967dfa
Ksenia Kovaleva <ks8niko@gmail.com>
2025-01-02 19:25:48 +0300

    добавлена обработка ошибок

--- app/src/main/java/com/example/playlistmaker/data/search/network/RetrofitNetworkClient.kt
+++ app/src/main/java/com/example/playlistmaker/data/search/network/RetrofitNetworkClient.kt
@@ -5,14 +5,19 @@ import com.example.playlistmaker.data.search.dto.Response
 import com.example.playlistmaker.data.search.dto.TrackSearchRequest
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import retrofit2.HttpException
 
 class RetrofitNetworkClient(private val searchApi: SearchApi) : NetworkClient {
 
     override suspend fun doRequest(dto: Any): Response {
         return if (dto is TrackSearchRequest) {
-            withContext(Dispatchers.IO) {
-                val response = searchApi.search(dto.expression)
-                response.apply { resultCode = 200 }
+            try {
+                withContext(Dispatchers.IO) {
+                    val response = searchApi.search(dto.expression)
+                    response.apply { resultCode = 200 }
+                }
+            } catch (e: HttpException) {
+                Response().apply { resultCode = e.code() }
             }
         } else {
             Response().apply { resultCode = 400 }
