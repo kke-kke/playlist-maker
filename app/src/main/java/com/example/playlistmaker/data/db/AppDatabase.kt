7d6e2c2f705bdacefbe79c80d066a00f3a2db22a
Ksenia Kovaleva <ks8niko@gmail.com>
2025-01-24 23:43:57 +0300

    добавлена бд

---
+++ app/src/main/java/com/example/playlistmaker/data/db/AppDatabase.kt
@@ -0,0 +1,11 @@
+package com.example.playlistmaker.data.db
+
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import com.example.playlistmaker.data.db.dao.TrackDao
+
+@Database(version = 1, entities = [TrackEntity::class])
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun getTrackDao(): TrackDao
+}
